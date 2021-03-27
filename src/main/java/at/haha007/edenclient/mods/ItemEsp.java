9576a67ce5157a49def7e29c1b388116611d2965
Haha007 <nichtechtanonym@gmail.com>
2021-03-27 18:03:02 +0100

    more itemesp stuff

--- src/main/java/at/haha007/edenclient/mods/ItemEsp.java
+++ src/main/java/at/haha007/edenclient/mods/ItemEsp.java
@@ -3,17 +3,18 @@ package at.haha007.edenclient.mods;
 import at.haha007.edenclient.callbacks.*;
 import at.haha007.edenclient.command.Command;
 import at.haha007.edenclient.command.CommandManager;
-import at.haha007.edenclient.utils.PlayerUtils;
 import at.haha007.edenclient.utils.RenderUtils;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL30;
+
+import static at.haha007.edenclient.utils.PlayerUtils.sendMessage;
+import static org.lwjgl.opengl.GL11.*;
 
 public class ItemEsp {
 	boolean enabled = false;
@@ -22,20 +23,20 @@ public class ItemEsp {
 
 	void initItemBox() {
 		if (itemBox != 0) return;
-		itemBox = GL11.glGenLists(1);
-		GL11.glNewList(itemBox, GL11.GL_COMPILE);
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		GL11.glDisable(GL11.GL_DEPTH_TEST);
-		GL11.glDisable(GL11.GL_LIGHTING);
-		GL11.glColor4f(1, 1, 0, 0.5F);
+		itemBox = glGenLists(1);
+		glNewList(itemBox, GL_COMPILE);
+		glEnable(GL_BLEND);
+		glDisable(GL_TEXTURE_2D);
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_LIGHTING);
+		glColor4f(1, 1, 0, 0.5F);
 		RenderUtils.drawOutlinedBox(new Box(-0.5, 0, -0.5, 0.5, 1, 0.5));
-		GL11.glEndList();
+		glEndList();
 	}
 
 	void destroy() {
 		if (itemBox == 0) return;
-		GL11.glDeleteLists(itemBox, 1);
+		glDeleteLists(itemBox, 1);
 		itemBox = 0;
 	}
 
@@ -61,64 +62,93 @@ public class ItemEsp {
 
 	private ActionResult onLoad(CompoundTag compoundTag) {
 		CompoundTag tag = compoundTag.getCompound("itemesp");
-		if (!tag.contains("enabled")) return ActionResult.PASS;
-		enabled = tag.getBoolean("enabled");
+		if (tag.contains("enabled")) enabled = tag.getBoolean("enabled");
+		if (tag.contains("size")) size = tag.getFloat("size");
 		return ActionResult.PASS;
 	}
 
 	private ActionResult onSave(CompoundTag compoundTag) {
 		CompoundTag tag = compoundTag.getCompound("itemesp");
 		tag.putBoolean("enabled", enabled);
+		tag.putFloat("size", size);
 		compoundTag.put("itemesp", tag);
 		return ActionResult.PASS;
 	}
 
 	private void onCommand(Command cmd, String label, String[] args) {
-		enabled = !enabled;
-		PlayerUtils.sendMessage(new LiteralText("Item ESP " + (enabled ? "enabled" : "disabled")));
+		if (args.length == 0) {
+			sendMessage(new LiteralText("/itemesp toggle").formatted(Formatting.GOLD));
+			sendMessage(new LiteralText("/itemesp size <size>").formatted(Formatting.GOLD));
+			return;
+		}
+		switch (args[0].toLowerCase()) {
+			case "toggle":
+				enabled = !enabled;
+				sendMessage(new LiteralText("Item ESP " + (enabled ? "enabled" : "disabled")));
+				break;
+
+			case "size":
+				if (args.length != 2) {
+					sendMessage(new LiteralText("/itemesp size <size>").formatted(Formatting.GOLD));
+					break;
+				}
+				try {
+					size = Float.parseFloat(args[1]);
+				} catch (NumberFormatException e) {
+					sendMessage(new LiteralText("Size has to be a number.").formatted(Formatting.GOLD));
+					break;
+				}
+				sendMessage(new LiteralText("Size: " + size).formatted(Formatting.GOLD));
+				break;
+
+			default:
+				sendMessage(new LiteralText("/itemesp toggle").formatted(Formatting.GOLD));
+				sendMessage(new LiteralText("/itemesp size <size>").formatted(Formatting.GOLD));
+				break;
+		}
 	}
 
 
 	private ActionResult renderItem(ItemEntity itemEntity, float yaw, float tickDelta, int light) {
 		if (!enabled) return ActionResult.PASS;
-		
-		// GL settings
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glEnable(GL11.GL_LINE_SMOOTH);
-		GL11.glLineWidth(2);
-		GL11.glDisable(GL11.GL_LIGHTING);
 
-		GL11.glPushMatrix();
+		// GL settings
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glEnable(GL_LINE_SMOOTH);
+		glLineWidth(2);
+		glDisable(GL_LIGHTING);
+
+		glPushMatrix();
 		RenderUtils.applyRegionalRenderOffset();
 
 		BlockPos camPos = RenderUtils.getCameraBlockPos();
 		int regionX = (camPos.getX() >> 9) * 512;
 		int regionZ = (camPos.getZ() >> 9) * 512;
 
-		GL11.glPushMatrix();
+		glPushMatrix();
 
-		GL11.glTranslated(
+		glTranslated(
 			itemEntity.prevX + (itemEntity.getX() - itemEntity.prevX) * tickDelta - regionX,
 			itemEntity.prevY + (itemEntity.getY() - itemEntity.prevY) * tickDelta,
 			itemEntity.prevZ + (itemEntity.getZ() - itemEntity.prevZ) * tickDelta - regionZ);
 
-		GL11.glPushMatrix();
-		GL11.glScaled(itemEntity.getWidth() + size,
-			itemEntity.getHeight() + size, itemEntity.getWidth() + size);
-		GL30.glCallList(itemBox);
-		GL11.glPopMatrix();
+		glPushMatrix();
 
+		glScaled(itemEntity.getWidth() + size,
+			itemEntity.getHeight() + size,
+			itemEntity.getWidth() + size);
+		glCallList(itemBox);
 
-		GL11.glPopMatrix();
-
-		GL11.glPopMatrix();
+		glPopMatrix();
+		glPopMatrix();
+		glPopMatrix();
 
 		// GL resets
-		GL11.glColor4f(1, 1, 1, 1);
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glDisable(GL11.GL_BLEND);
-		GL11.glDisable(GL11.GL_LINE_SMOOTH);
+		glColor4f(1, 1, 1, 1);
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_TEXTURE_2D);
+		glDisable(GL_BLEND);
+		glDisable(GL_LINE_SMOOTH);
 
 		return ActionResult.PASS;
 	}
