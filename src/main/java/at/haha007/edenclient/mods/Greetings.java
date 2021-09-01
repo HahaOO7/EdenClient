b742f8aa4934ef6dba1148b5d5326e8c12d6addc
Haha007 <nichtechtanonym@gmail.com>
2021-09-01 12:56:37 +0200

    greeting -> wb

--- src/main/java/at/haha007/edenclient/mods/Greetings.java
+++ src/main/java/at/haha007/edenclient/mods/Greetings.java
@@ -19,8 +19,11 @@ import static at.haha007.edenclient.command.CommandManager.*;
 public class Greetings {
     private boolean enabled;
     private int minDelay;
+    private int wbDelay;
     private Set<String> sentPlayers;
+    private Set<String> quitPlayers;
     private String newPlayer;
+    private String wbText;
     private String oldPlayer;
 
     public Greetings() {
@@ -33,25 +36,32 @@ public class Greetings {
     private void load(NbtCompound nbtCompound) {
         NbtCompound tag = nbtCompound.getCompound("greeting");
         sentPlayers = new HashSet<>();
+        quitPlayers = new HashSet<>();
         if (tag.isEmpty()) {
             enabled = false;
             minDelay = 1200;
+            wbDelay = 1200;
             newPlayer = "Hey %player%";
             oldPlayer = "Hey %player%";
             return;
         }
         enabled = tag.getBoolean("enabled");
         minDelay = tag.getInt("delay");
+        wbDelay = Math.min(tag.getInt("wbDelay"), 20);
         newPlayer = tag.getString("newPlayer");
         oldPlayer = tag.getString("oldPlayer");
+        wbText = tag.getString("wbText");
+
     }
 
     private void save(NbtCompound nbtCompound) {
         NbtCompound tag = new NbtCompound();
         tag.putBoolean("enabled", enabled);
         tag.putInt("delay", minDelay);
+        tag.putInt("wbDelay", wbDelay);
         tag.putString("newPlayer", newPlayer);
         tag.putString("oldPlayer", oldPlayer);
+        tag.putString("wbText", wbText);
 
         nbtCompound.put("greeting", tag);
     }
@@ -60,10 +70,20 @@ public class Greetings {
         if (!enabled) return;
         String msg = chatAddEvent.getChatText().getString();
         if (msg.length() < 5) return;
+
+        if (msg.startsWith("[-] ")) {
+            String name = msg.substring(4);
+            quitPlayers.add(name);
+            Scheduler.get().scheduleSyncDelayed(() -> quitPlayers.remove(name), wbDelay);
+        }
+
         if (msg.startsWith("[+] ")) {
             String name = msg.substring(4);
-            addDelay(name, oldPlayer);
-        } else if (msg.substring(1).startsWith(" Willkommen, ") && msg.endsWith(" :) Viel spaß auf openMC!")) {
+            if (quitPlayers.contains(name))
+                addDelay(name, wbText);
+            else
+                addDelay(name, oldPlayer);
+        } else if (msg.substring(1).startsWith(" Willkommen, ") && msg.endsWith(" :) Viel Spaß auf OpenMC!")) {
             String name = msg.substring(14, msg.length() - 25);
             addDelay(name, newPlayer);
         }
@@ -72,7 +92,6 @@ public class Greetings {
     private void addDelay(String name, String message) {
         if (sentPlayers.contains(name)) return;
         sentPlayers.add(name);
-        System.out.println(name);
         Scheduler.get().scheduleSyncDelayed(() -> {
             PlayerUtils.messageC2S(message.replace("%player%", name));
             Scheduler.get().scheduleSyncDelayed(() -> sentPlayers.remove(name), minDelay);
@@ -81,21 +100,37 @@ public class Greetings {
 
     private void registerCommand() {
         LiteralArgumentBuilder<ClientCommandSource> cmd = literal("greet");
+
         cmd.then(literal("new").then(argument("text", StringArgumentType.greedyString()).executes(c -> {
             newPlayer = c.getArgument("text", String.class);
             PlayerUtils.sendModMessage("Message updated");
             return 1;
         })));
+
         cmd.then(literal("old").then(argument("text", StringArgumentType.greedyString()).executes(c -> {
             oldPlayer = c.getArgument("text", String.class);
             PlayerUtils.sendModMessage("Message updated");
             return 1;
         })));
+
+        cmd.then(literal("wb").then(argument("text", StringArgumentType.greedyString()).executes(c -> {
+            wbText = c.getArgument("text", String.class);
+            PlayerUtils.sendModMessage("Message updated");
+            return 1;
+        })));
+
         cmd.then(literal("delay").then(argument("delay", IntegerArgumentType.integer(1)).executes(c -> {
             minDelay = c.getArgument("delay", Integer.class) * 20;
             PlayerUtils.sendModMessage("Minimal delay updated");
             return 1;
         })));
+
+        cmd.then(literal("wbdelay").then(argument("delay", IntegerArgumentType.integer(1)).executes(c -> {
+            wbDelay = c.getArgument("delay", Integer.class) * 20;
+            PlayerUtils.sendModMessage("Wb delay updated");
+            return 1;
+        })));
+
         cmd.then(literal("toggle").executes(c -> {
             enabled = !enabled;
             PlayerUtils.sendModMessage(enabled ? "Greetings enabled" : "Greetings disabled");
