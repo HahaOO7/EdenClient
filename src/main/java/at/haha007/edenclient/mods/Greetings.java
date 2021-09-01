80a41c2b06e0decded229d4ab887ab8d2fc260d1
w1tcherrr <mischa.szasz@gmx.net>
2021-09-01 16:20:46 +0200

    Add list for ignored players in Greetings. Fix crash on save due to wbText being null.

--- src/main/java/at/haha007/edenclient/mods/Greetings.java
+++ src/main/java/at/haha007/edenclient/mods/Greetings.java
@@ -8,11 +8,20 @@ import at.haha007.edenclient.utils.Scheduler;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.minecraft.client.network.ClientCommandSource;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Formatting;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import static at.haha007.edenclient.command.CommandManager.*;
 
@@ -22,6 +31,7 @@ public class Greetings {
     private int wbDelay;
     private Set<String> sentPlayers;
     private Set<String> quitPlayers;
+    private Set<String> ignoredPlayers;
     private String newPlayer;
     private String wbText;
     private String oldPlayer;
@@ -37,12 +47,14 @@ public class Greetings {
         NbtCompound tag = nbtCompound.getCompound("greeting");
         sentPlayers = new HashSet<>();
         quitPlayers = new HashSet<>();
+        ignoredPlayers = new HashSet<>();
         if (tag.isEmpty()) {
             enabled = false;
             minDelay = 1200;
             wbDelay = 1200;
             newPlayer = "Hey %player%";
             oldPlayer = "Hey %player%";
+            wbText = "Wb %player%";
             return;
         }
         enabled = tag.getBoolean("enabled");
@@ -51,7 +63,7 @@ public class Greetings {
         newPlayer = tag.getString("newPlayer");
         oldPlayer = tag.getString("oldPlayer");
         wbText = tag.getString("wbText");
-
+        ignoredPlayers = tag.getList("ignoredPlayers", 10).stream().map(nbt -> (NbtCompound) nbt).map(nbtCompound1 -> nbtCompound1.getString("player")).collect(Collectors.toSet());
     }
 
     private void save(NbtCompound nbtCompound) {
@@ -62,7 +74,13 @@ public class Greetings {
         tag.putString("newPlayer", newPlayer);
         tag.putString("oldPlayer", oldPlayer);
         tag.putString("wbText", wbText);
-
+        NbtList ignoredPlayersComp = new NbtList();
+        ignoredPlayers.forEach(s -> {
+            NbtCompound playerComp = new NbtCompound();
+            playerComp.putString("player", s);
+            ignoredPlayersComp.add(playerComp);
+        });
+        tag.put("ignoredPlayers", ignoredPlayersComp);
         nbtCompound.put("greeting", tag);
     }
 
@@ -73,6 +91,7 @@ public class Greetings {
 
         if (msg.startsWith("[-] ")) {
             String name = msg.substring(4);
+            if (ignoredPlayers.contains(name.toLowerCase())) return;
             quitPlayers.add(name);
             Scheduler.get().scheduleSyncDelayed(() -> quitPlayers.remove(name), wbDelay);
         }
@@ -136,6 +155,53 @@ public class Greetings {
             PlayerUtils.sendModMessage(enabled ? "Greetings enabled" : "Greetings disabled");
             return 1;
         }));
+
+        cmd.then(literal("ignore").then(literal("add").then(argument("player", StringArgumentType.word()).executes(c -> {
+            String player = c.getArgument("player", String.class);
+            if (!player.matches("[A-Za-z0-9_]{4,16}")) {
+                PlayerUtils.sendModMessage("Your input is not a valid username.");
+                return 0;
+            }
+            ignoredPlayers.add(player.toLowerCase());
+            PlayerUtils.sendModMessage(new LiteralText("Added ").formatted(Formatting.GOLD)
+                    .append(new LiteralText(player).formatted(Formatting.AQUA))
+                    .append(new LiteralText(" to ignored players.").formatted(Formatting.GOLD)));
+            return 1;
+        }))));
+
+        cmd.then(literal("ignore").then(literal("remove").then(argument("player", StringArgumentType.word()).suggests(this::suggestIgnoredPlayers).executes(c -> {
+            String player = c.getArgument("player", String.class);
+            if (ignoredPlayers.remove(player.toLowerCase())) {
+                PlayerUtils.sendModMessage(new LiteralText("Successfully removed ").formatted(Formatting.GOLD)
+                        .append(new LiteralText(player).formatted(Formatting.AQUA))
+                        .append(" from your ignored players.").formatted(Formatting.GOLD));
+                return 1;
+            } else {
+                PlayerUtils.sendModMessage("This player is not ignored and can therefore not be removed.");
+                return 0;
+            }
+        }))));
+
+        cmd.then(literal("ignore").then(literal("list").executes(c -> {
+            if (ignoredPlayers.size() == 0) {
+                PlayerUtils.sendModMessage("No players ignored for Greetings.");
+                return 0;
+            }
+
+            List<String> ignoredPlayersList = ignoredPlayers.stream().sorted().collect(Collectors.toList());
+            PlayerUtils.sendModMessage("Ignored players for Greetings:");
+            for (int i = 0; i < ignoredPlayers.size(); i++) {
+                PlayerUtils.sendModMessage(new LiteralText("[" + (i + 1) + "] ").formatted(Formatting.GOLD)
+                        .append(new LiteralText(ignoredPlayersList.get(i)).formatted(Formatting.AQUA)));
+            }
+            return 1;
+        })));
+
         register(cmd);
+    }
+
+    private CompletableFuture<Suggestions> suggestIgnoredPlayers(CommandContext<ClientCommandSource> clientCommandSourceCommandContext, SuggestionsBuilder suggestionsBuilder) {
+        ignoredPlayers.forEach(suggestionsBuilder::suggest);
+        return suggestionsBuilder.buildFuture();
     }
 }
